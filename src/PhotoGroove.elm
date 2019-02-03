e529b684630edecd24f42b3acd11fffebe85fab9
PrasannaDnae <gnanaprasanna.gnanaraj@gmail.com>
2019-02-03 19:28:18 +0000

    expanding model alter thumbnail func

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -11,19 +11,26 @@ urlPrefix =
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ] (List.map viewThumbnail model.photos)
         ]
 
 
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.url) ] []
+viewThumbnail selectedUrl thumb =
+    if selectedUrl == thumb.url then
+        img [ src (urlPrefix ++ thumb.url), class "selected" ] []
+
+    else
+        img [ src (urlPrefix ++ thumb.url) ] []
 
 
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
 
 
 main =
